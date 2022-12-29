8d7fa728d2f6e18a55d79f5b75626e411b9f5551
Imed Triki <imedtriki55@gmail.com>
2022-12-29 13:19:46 -0800

    quelques modifications dans le navbar et ajout de RemplirInfoUser

--- FRONTEND/src/Pages/Home.js
+++ FRONTEND/src/Pages/Home.js
@@ -7,6 +7,9 @@ import pic4 from '../images/pic4.jpeg'
 import pic5 from '../images/pic5.jpeg'
 import { useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { Drawer } from '@mui/material'
+import Box from '@mui/material/Box';
+import { FcShop } from 'react-icons/fc';
 
 const Home = () => {
     const slides = [
@@ -39,19 +42,56 @@ const Home = () => {
         const newIndex = isLastSlide ? 0 : currentIndex + 1;
         setCurrentIndex(newIndex);
     };
+
+    const [state, setState] = React.useState({
+        right: false,
+    });
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
     return (
         <div className='flex flex-col h-full' style={{ "backgroundColor": "#f5f5f5" }}>
             <div className="w-full flex flex-col sticky top-0  ">
                 <SideBar />
             </div>
+            <React.Fragment key="right">
+                <Drawer className=" h-screen w-72 sticky rounded-md top-0" anchor='right' open={state["right"]} onClose={toggleDrawer("right", false)} >
+                    <Box sx={{ width: 350 }} className="h-full" >
+                        <div className='h-full flex items-center flex-col justify-between divide-y divide-gray-300'>
+                            <div className='w-full h-16 text-lg flex flex-row justify-around items-center top-0 bg-gray-100 font-bold text-gray-900 shadow'>
+                                <div></div>
+                                <div className='flex flex-row items-center '>
+                                    <div className='cursor-pointer mr-2'>
+                                        <FcShop size={30} />
+                                    </div>
+                                    Nom d'annoceur
+                                </div>
+                                <div className='cursor-pointer' onClick={toggleDrawer("right", false)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </div>
+                            </div>
+                            <div className='flex flex-row items-center p-2 w-full'>
+                                <input type="text" className="bg-gray-50 border border-gray-500 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Ecrire" required />
+                                <svg aria-hidden="true" className="w-8 h-8 rotate-45 ml-2 cursor-pointer" fill="blue" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
+                            </div>
+                        </div>
+                    </Box>
+                </Drawer>
+            </React.Fragment>
             <div className='mt-8 h-screen'>
                 <div className="grid grid-cols-4 gap-3 m-8">
-                    <div className=" cursor-pointer bg-white max-w-xs  rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+                    <div className=" cursor-pointer bg-white max-w-xs  rounded-lg shadow-md ">
                         <div className=" flex flex-col">
                             <div className='  w-full h-72 group'>
                                 <div
                                     style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-                                    className='w-full flex-col justify-between flex items-center h-full rounded-md bg-center bg-cover duration-500'
+                                    className='w-full flex-col justify-between flex items-center h-full rounded-sm bg-center bg-cover duration-500'
                                 >
                                     <div className='flex w-full justify-between flex-row p-3 '>
                                         <div></div>
@@ -75,17 +115,25 @@ const Home = () => {
                             </div>
                             <div className=' divide-y divide-gray-300'>
                                 <div className='p-3' >
-                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">In the night Apart</h5>
-                                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">4 Bedrooms - 2 Bathrooms - 2.5M</p>
-                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">$753,00/<span className='text-gray-700 font-normal'>Year</span></h5>
+                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 "> Location Local Alger Bordj el bahri </h5>
+                                    <div className='flex flex-row items-center justify-between '>
+                                        <h5 className=" text-2xl  tracking-tight text-[#e95903] ">6 millions</h5>
+                                    </div>
                                 </div>
-
+                                <div className='flex flex-row justify-between py-2 px-3'>
+                                    <div>environ 4 jours</div>
+                                    <div onClick={toggleDrawer("right", true)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+                                        </svg>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
