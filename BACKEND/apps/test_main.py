5d38694f2a8671135ac99f36f761b3943d72b417
Nayluche <nailaboudedja@gmail.com>
2023-02-04 19:41:42 +0100

    ajout du dernier test unitaire

--- BACKEND/apps/test_main.py
+++ BACKEND/apps/test_main.py
@@ -21,3 +21,9 @@ def test_CreerUser():
     response = client.post('/user',json={"name":"naila","email":"nail@gmailcom","numeroDeTelephone":"26","token":"ok","adresse":"rue"})
     test = client.get(f'/user/{response.json()["id"]}')
     assert response.json() == test.json()
+
+def test_CreerAnnonce():
+    data = {"request":{"titre":"annonce","categories":"vente","typeDuBien":"Bureau","surfaces":"2","description":"ok","prix":"10000DA","localisation":"alger"},"userid":1,"paths":["item1","item2"]}
+    response = client.post('/annonce',json=data)
+    test = client.get(f'/annonce/{response.json().get("id")}')
+    assert response.json().get("id") == test.json().get("id")
